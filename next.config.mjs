cc5c0dab9c7029e1011599291ccdcd79c61109a3
Nguyen Minh Tuan (k18 HCM) <TuanNMSE184386@fpt.edu.vn>
2025-03-22 00:54:20 +0700

    feat: config domain for images from pexels

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
     unoptimized: process.env.NODE_ENV === 'development',
   },
   // Thêm cấu hình cho Vercel
@@ -8,3 +15,5 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
